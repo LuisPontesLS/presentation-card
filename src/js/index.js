df2d5c0629a1c2ee94aa6c75b039812da78eecda
LuisPontesLS <leospontes@hotmail.com>
2022-10-25 16:10:53 -0300

    Add o JS

---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+/*
+OBJETIVO FINAL-  quando clicarmos na aba, temos que mostrar
+o conteúdo da aba que foi clicada pelo usuário e esconder
+o conteúdo da outra aba.
+
+1 - dar um jeito de pegar es elementos HTML que 
+representam as abas;
+
+2 - dar um jeito de identificar o clique no elemento
+que represnta a aba;
+
+3 - quando o usuário clicar, desmarcar a outra aba;
+
+4 - marcar a aba clicada como "selecionado";
+
+5 - esconder o conteúdo anterios;
+
+6 - mostrar o conteúdo da aba selecionada.
+ */
+//=================================================================
+/*1 - dar um jeito de pegar es elementos HTML que 
+representam as abas; CADA ABA É UMA 'LI' COM UMA
+.aba*/
+
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    // 2 - dar um jeito de identificar o clique no elemento que represnta a aba;
+    aba.addEventListener("click", function () {
+        if (aba.classList.contains("selecionado")) {
+            return;
+        }
+
+        // console.log("clicou na aba", aba);
+        /* 3 - quando o usuário clicar, desmarcar a outra aba;
+        VAI USAR A 'DOM' PARA DEMOVER A CLASS selecionado (linha 42 html)*/
+        const abaSelecionada = document.querySelector(".aba.selecionado")
+        abaSelecionada.classList.remove("selecionado")
+        // 4 - marcar a aba clicada como "selecionado";
+        aba.classList.add("selecionado")
+        // 5 - esconder o conteúdo anterios;
+        const informacaoSelecionada = document.querySelector(".informacao.selecionado")
+        console.log(informacaoSelecionada);
+        //escondendo o conteúdo:
+        informacaoSelecionada.classList.remove("selecionado")
+
+        // 6 - mostrar o conteúdo da aba selecionada.
+        //1:48
+    })
+})
